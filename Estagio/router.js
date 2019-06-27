e6a2d123328b1afd2481ed4c042dc75501e621d4
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-06-26 22:44:37 -0500

    Update router.js

--- Estagio/router.js
+++ Estagio/router.js
@@ -7,10 +7,14 @@ import MenuInterno from './src/interfaces/MenuInterno';
 import Leis from './src/interfaces/Leis';
 import TelaCadastroProblema from './src/interfaces/TelaCadastroProblema';
 import TelaLogin from './src/interfaces/TelaLogin';
-import TelaCadastroUsuario from './src/interfaces/TelaCadastroUsuario'
+import TelaCadastroUsuario from './src/interfaces/TelaCadastroUsuario';
+import Lista_Materia from './src/interfaces/Lista_Materia'
 import SAPL from './src/interfaces/SAPL';
 import Sobre from './src/interfaces/Sobre';
-
+import TelaAlteraDadosUsuario from './src/interfaces/TelaAlteraDadosUsuario'
+import TelaAlteraSenhaUsuario from './src/interfaces/TelaAlteraSenhaUsuario'
+import Materia_Legislativa from './src/interfaces/Materia_Legislativa'
+import TelaCarregamento from './src/interfaces/TelaCarregamento'
 
 
 export default class routes extends React.Component {
@@ -22,6 +26,10 @@ export default class routes extends React.Component {
 					key="root"
 					navBarButtonColor={'#FFFFFF'}
 					headerLayoutPreset='center'>
+				<Scene
+					hideNavBar
+					key="TelaCarregamento"
+					component={TelaCarregamento} />
 					<Drawer
 						hideNavBar
 						key="MenuExterno"
@@ -66,17 +74,35 @@ export default class routes extends React.Component {
 						title='Cadastro de Problema'
 						key='TelaCadastroProblema'
 						component={TelaCadastroProblema} />
-				<Scene
+					<Scene
 						title='Pesquisar Matéria Legislativa'
 						key='SAPL'
 						component={SAPL} />
-				<Scene
+					<Scene
 						title='Sobre'
 						key='Sobre'
 						component={Sobre} />
-	
+					<Scene
+						title='Alterar dados'
+						key='TelaAlteraDadosUsuario'
+						component={TelaAlteraDadosUsuario} />
+					<Scene
+						title='Alterar Senha'
+						key='TelaAlteraSenhaUsuario'
+						component={TelaAlteraSenhaUsuario} />	
+					<Scene
+						title='Materia Legislativa'
+						key='Materia_Legislativa'
+						component={Materia_Legislativa} />
+					<Scene
+						title='Matérias Legislativas'
+						key='Lista_Materia'
+						component={Lista_Materia} />
+						<Scene
+						title='Tela Carregamento'
+						key='TelaCarregamento'
+						component={TelaCarregamento} />
 				</Scene>
-				
 			</Router>
 		);
 	}

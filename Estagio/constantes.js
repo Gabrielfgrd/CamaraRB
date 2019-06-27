bd6a9706cbd7061e2e0bd498d04399232e8e325c
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-06-26 22:44:08 -0500

    Create constantes.js

---
+++ Estagio/constantes.js
@@ -0,0 +1,20 @@
+export const constUsuario = {
+    modificaNome : 'modifica_nome',
+    modificaEndereco : 'modifica_endereco',
+    modificaCpf : 'modifica_cpf',
+    modificaEmail : 'modifica_email',
+    modificaSenha : 'modifica_senha',
+    modificaSenha2 : 'modifica_senha2',
+
+    modificaEdiNome : 'modifica_ediNome',
+    modificaEdiEndereco : 'modifica_ediEndereco',
+    modificaEdiCpf : 'modifica_ediCpf',
+    modificaEdiEmail : 'modifica_ediEmail',
+    modificaEdiSenha : 'modifica_ediSenha',
+    modificaEdiSenha2 : 'modifica_ediSenha2',
+
+    autenticacaoSucesso : 'autenticacao_sucesso',
+    igualaDadosEdicao : 'igualaDadosEdicao',
+    edicaoSucesso : 'edicao_sucesso'
+
+}

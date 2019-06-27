af86bceb789f2ab3c52ee3f3417170054736c672
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-06-26 22:44:31 -0500

    Update index.js

--- Estagio/reducers/index.js
+++ Estagio/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import UsuarioReducer from './UsuarioReducer'
+import ProblemaReducer from './ProblemaReducer'
 
 export default combineReducers({
+    ProblemaReducer,
     UsuarioReducer
 });

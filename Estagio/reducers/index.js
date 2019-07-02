ed029df7e4dcbf673f0716c8c61501a233a5a0ea
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-07-02 16:12:08 -0500

    Update index.js

--- Estagio/reducers/index.js
+++ Estagio/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import UsuarioReducer from './UsuarioReducer'
 import ProblemaReducer from './ProblemaReducer'
+import YouTubeReducer from './YouTubeReducer'
 
 export default combineReducers({
     ProblemaReducer,
-    UsuarioReducer
+    UsuarioReducer, 
+    YouTubeReducer
 });

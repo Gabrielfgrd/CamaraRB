0e0c28ebe8974486cb226223d275ab29561e4424
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-07-02 16:11:39 -0500

    Create YouTubeActions.js

---
+++ Estagio/actions/YouTubeActions.js
@@ -0,0 +1,15 @@
+import { Actions } from 'react-native-router-flux'
+import _ from 'lodash';
+
+export const modificaId = (texto) => {
+    return {
+        type: 'modifica_id',
+        payload: texto
+    }
+}
+export const modificaVideoId = (texto) => {
+    return {
+        type: 'modifica_videoId',
+        payload: texto
+    }
+}

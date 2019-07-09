aa983c7a1351f2a00bb873489e1a5bde065351dd
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-07-09 10:36:32 -0500

    Update TelaCadastroProblema.js

--- Estagio/src/interfaces/TelaCadastroProblema.js
+++ Estagio/src/interfaces/TelaCadastroProblema.js
@@ -7,68 +7,63 @@ import { connect } from 'react-redux';
 import { modificaTipoDeProblemaId, modificaDescricao, modificaDataCriacao, modificaBairro,modificaRua,  inclusaoProblema, recuperaTiposDeProblemas } from '../../actions/ProblemasActions';
 
 
+
 class TelaCadastroProblema extends React.Component {
-			
 			constructor(props) {
 				super(props);
+				this.props.recuperaTiposDeProblemas()
 				now = new Date
 				this.props.modificaDataCriacao(now.getDate() + '/' + (now.getMonth()+1) + '/' + now.getFullYear())
-				//this.props.modificaTipoDeProblemaId(this.props.tiposDeProblemas[0].id)
-				this.props.recuperaTiposDeProblemas()
+				// this.props.modificaTipoDeProblemaId(this.props.tiposDeProblemas[0].id)
 				this.state = {
-					tipoDeProblemaSelecionado: this.props.tiposDeProblemas[0].id
+					tipoDeProblemaSelecionado: ''
 				}
-				console.log(this.props.tiposDeProblemas)
 			}
 			_inclusaoDeProblema() {
 				if(this.props.descricao!=''){
-					Actions.TelaCarregamento();
 					const { descricao, tipoDeProblemaId, dataCriacao, bairro, rua } = this.props
 					this.props.inclusaoProblema({ descricao, tipoDeProblemaId, dataCriacao, bairro, rua })
 				}else{
 					alert('Escreva uma descrição do problema, isso ajudará na identificação dele.')
 				}
 			}
-		
-
 
 	render(){
+
 		return(
 			
 				<ScrollView style={{flex: 1, padding: 10, marginTop:25}}>
 					<View style={{flex: 2, alignItems: 'center', marginTop: 15, fontSize: 20 }}>
 						<Text style={styles.textoPadrao}>Tipo de problema:</Text>
 						<Picker
-						selectedValue ={()=>{this.state.tipoDeProblemaSelecionado
-							
-						} } 
+						selectedValue ={this.state.tipoDeProblemaSelecionado } 
 						onValueChange={(itemValue) => {
 							this.setState({ tipoDeProblemaSelecionado: itemValue})
 							this.props.modificaTipoDeProblemaId(itemValue)
 						}}
 						style={{ height: 60, width: 200, color: colors.preto }}
 						>
-							{this.props.tiposDeProblemas.map(tipoDeProblema => (
-								<Picker.Item
-									label={tipoDeProblema.titulo}
-									value={tipoDeProblema.id}
-								/>
-							))}
+						{this.props.tiposDeProblemas.map(tipoDeProblema => (
+							<Picker.Item
+								label={tipoDeProblema.titulo}
+								value={tipoDeProblema.id}
+							/>
+						))}
 						</Picker>
 					</View >
 					<View style={{flex: 3, alignItems: 'center', marginTop: 15, fontSize: 20 }}>
 						<Text style={styles.textoPadrao}>Rua:</Text>
-						<TextInput maxLength={200} multiline={true} style={styles.textArea} placeholder='Rua'/>
+						<TextInput maxLength={200}  value={this.props.rua} multiline={true} style={styles.textArea} placeholder='Rua' onChangeText={texto => this.props.modificaRua(texto)}/>
 						<Text style={styles.textoPadrao}>Bairro:</Text>
-						<TextInput maxLength={200} multiline={true} style={styles.textArea} placeholder='Bairro'/>
+						<TextInput maxLength={200}  value={this.props.bairro} multiline={true} style={styles.textArea} placeholder='Bairro' onChangeText={texto => this.props.modificaBairro(texto)}/>
 						<Text style={styles.textoPadrao}>Descreva o problema:</Text>
-						<TextInput maxLength={200} multiline={true} style={styles.textArea} placeholder='Descreva o Problema'/>
+						<TextInput maxLength={200}  value={this.props.descricao} multiline={true} style={styles.textArea} placeholder='Descreva o Problema' onChangeText={texto => this.props.modificaDescricao(texto)}/>
 					</View>
 					<View style={{flex: 1, alignItems: 'center',justifyContent: 'center', marginTop: 30}}>
 						<TouchableOpacity
-						OnPress={ ()=>{this.salvarProblemas(); }}
+						OnPress={ ()=>{this._inclusaoDeProblema(); }}
 						style={styles.botao}>
-						<Text style={styles.textoBotao}> Cadastrar </Text>
+						<Text style={styles.textoBotao}> Inserir Problema </Text>
 						</TouchableOpacity>
 					</View>
 				</ScrollView>

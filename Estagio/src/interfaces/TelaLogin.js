a59cb54d93fff0c73bf3100644bfe4d488431f4c
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-08-07 17:37:13 -0500

    Update TelaLogin.js

--- Estagio/src/interfaces/TelaLogin.js
+++ Estagio/src/interfaces/TelaLogin.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Text, TextInput, TouchableHighlight,TouchableOpacity} from 'react-native';
+import { View, Text, TextInput, TouchableHighlight, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import {styles, colors} from '../Components/layout';
+import { styles, colors } from '../Components/layout';
 import {
-	modificaEmail,
+    modificaEmail,
     modificaSenha,
     autenticaUsuario
 } from '../../actions/UsuariosActions'
@@ -12,41 +12,41 @@ import { connect } from 'react-redux';
 
 
 class TelaLogin extends React.Component {
-	_autenticaUsuario() {
-		Actions.TelaCarregamento();
-		const { email, senha } = this.props
-		this.props.autenticaUsuario({ email, senha })
-	}
-	render() {
-		return (
-			<View style ={{ flex: 1, padding: 10}}>
-                <View style ={{flex: 3, alignItems: 'center', justifyContent: 'center', marginTop: 60}}>
+    _autenticaUsuario() {
+        Actions.TelaCarregamento();
+        const { email, senha } = this.props
+        this.props.autenticaUsuario({ email, senha })
+    }
+    render() {
+        return (
+            <View style={{ flex: 1, padding: 10 }}>
+                <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center', marginTop: 60 }}>
                     <Text style={styles.textoPadrao} >E-mail</Text>
-                    <TextInput style={styles.textInput} value={this.props.email} placeholder = "E-mail" onChangeText={texto => this.props.modificaEmail(texto)}/>
+                    <TextInput style={styles.textInput} value={this.props.email} placeholder="E-mail" onChangeText={texto => this.props.modificaEmail(texto)} />
                     <Text style={styles.textoPadrao} >Senha</Text>
-                    <TextInput style={styles.textInput} value={this.props.senha} secureTextEntry placeholder = "Senha" onChangeText={texto => this.props.modificaSenha(texto)}/>
-                <View style ={{flex: 3, alignItems: 'center', justifyContent: 'center', manginTop: 30}} >
-                    <Text style={styles.textoPadrao} >Ainda não tem cadastro? </Text>
-                <TouchableHighlight onPress={() => Actions.TelaCadastroUsuario() }>
-                    <Text style ={{fontSize: 20, textDecorationLine: 'underline', color: '#000000'} }>Cadastre-se</Text>
-                    </TouchableHighlight>
+                    <TextInput style={styles.textInput} value={this.props.senha} secureTextEntry placeholder="Senha" onChangeText={texto => this.props.modificaSenha(texto)} />
+                    <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center', manginTop: 30 }} >
+                        <Text style={styles.textoPadrao} >Ainda não tem cadastro? </Text>
+                        <TouchableHighlight onPress={() => Actions.TelaCadastroUsuario()}>
+                            <Text style={{ fontSize: 20, textDecorationLine: 'underline', color: '#000000' }}>Cadastre-se</Text>
+                        </TouchableHighlight>
                     </View>
                 </View>
-                <View style ={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
                     <TouchableOpacity
                         style={styles.botao}
-                        onPress={()=>{ this._autenticaUsuario()}}>
+                        onPress={() => { this._autenticaUsuario() }}>
                         <Text style={styles.textoBotao}> Entrar </Text>
                     </TouchableOpacity>
                 </View>
             </View>
-		);
-	}
+        );
+    }
 }
 const mapStateToProps = state => (
-	{
-		email: state.UsuarioReducer.email,
-		senha: state.UsuarioReducer.senha
-	}
+    {
+        email: state.UsuarioReducer.email,
+        senha: state.UsuarioReducer.senha
+    }
 )
-export default connect(mapStateToProps, { modificaEmail, modificaSenha, autenticaUsuario})(TelaLogin);
+export default connect(mapStateToProps, { modificaEmail, modificaSenha, autenticaUsuario })(TelaLogin);

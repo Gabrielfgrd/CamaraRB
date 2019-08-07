9c037d4482a9b430b9137394c6018cf53e599ef1
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-08-07 17:37:16 -0500

    Update TelaDeProblemas.js

--- Estagio/src/interfaces/TelaDeProblemas.js
+++ Estagio/src/interfaces/TelaDeProblemas.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Image,
-  View,
-  Text,
-  ScrollView,
-  TouchableHighlight,
+    StyleSheet,
+    Image,
+    View,
+    Text,
+    ScrollView,
+    TouchableHighlight,
 } from 'react-native';
 import {
-	modificaTipoDeProblemaId,
-	modificaDescricao,
-	modificaDataCriacao,
-	modificaBairro,
+    modificaTipoDeProblemaId,
+    modificaDescricao,
+    modificaDataCriacao,
+    modificaBairro,
     modificaRua,
     recuperaTiposDeProblemas,
     recuperaTodosOsProblemas,
@@ -20,28 +20,23 @@ import {
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 
-class TelaDeProblema extends Component {
-    constructor (props){
-            super(props);
-            this.props.recuperaTiposDeProblemas()
-            this.props.recuperaTodosOsProblemas()
-        
+
+
+export default class TelaDeProblema extends Component {
+    constructor(props) {
+        super(props);
+        this.props.recuperaTiposDeProblemas()
+        this.props.recuperaTodosOsProblemas()
+
     }
-    componentWillMount(){
+    componentWillMount() {
         this.props.recuperaTodosOsProblemas();
     }
 
 }
-
- mapStateToProps = state =>
-    {
-    const contatos = _map(state.ProblemaReducers, (val,uid) => {
-        return {val,uid}
-    })
-    console.log(contatos);
-    return{}
-    }
-export default connect(mapStateToProps, 
-    {  
-        recuperaTodosOsProblemas })
-(TelaDeProblemas);
+mapStateToProps = state => {
+    console.log(state)
+    return {}
+}
+export default connect(mapStateToProps, { recuperaTodosOsProblemas })
+    (TelaDeProblemas);

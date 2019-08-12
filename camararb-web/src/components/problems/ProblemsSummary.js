e7a43d6e17aaaba57a8e65f22a4ea637408fd040
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-08-12 16:17:50 -0500

    Update ProblemsSummary.js

--- camararb-web/src/components/problems/ProblemsSummary.js
+++ camararb-web/src/components/problems/ProblemsSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProblemsSummary = () => {
+const ProblemsSummary = ({problem}) => {
     return (
         <div className="card z-depth-0 sumario-problema">
             <div className="card-content indigo-text text-darken-2 ">
-                <span className="card-title"> Titulo do Problema</span>
+                <span className="card-title">{problem.titulo}</span>
                 <p>Postado pelo Gabriel</p>
                 <p className="indigo-text">3 de Janeiro, 12:00</p>
             </div>

4556697d0ace76405336b170789508d6f3287ffc
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-08-12 16:17:32 -0500

    Update Dashboard.js

--- camararb-web/src/components/dashboard/Dashboard.js
+++ camararb-web/src/components/dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications'
 import ProblemsList from '../problems/ProblemsList'
+import {connect} from 'react-redux'
 
 class Dashboard extends Component {
     render() {
+        // console.log(this.props);
+        const { problems} = this.props;
+
         return (
             <div className="dashboard.container">
                 <div className="row">
                     <div className="col s12 m6">
-                          <ProblemsList />
+                          <ProblemsList problems={problems} />
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -19,4 +23,10 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+    return{
+        problems: state.problem.problems
+    }
+}
+
+export default connect(mapStateToProps) (Dashboard)

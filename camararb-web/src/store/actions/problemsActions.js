69c427ed742ea55d1fe914112ea6ac807b9065e5
Gabrielfgrd <44777884+Gabrielfgrd@users.noreply.github.com>
2019-08-20 15:49:00 -0500

    Update problemsActions.js

--- camararb-web/src/store/actions/problemsActions.js
+++ camararb-web/src/store/actions/problemsActions.js
@@ -1,6 +1,26 @@
+import Firebase from 'firebase';
+
+
 export const createProblem = (problem) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         //faz uma chamada assincrona ao bd
-        dispatch({ type: 'CREATE_PROBLEM', problem});
+        // const firestore = getFirestore();
+        // firestore.collection('problemas').add({  
+        const novoProblema = Firebase.database().ref().child('/problemas').push()
+        novoProblema.set({
+            ...problem,
+            id: novoProblema.key,
+            titulo: 'TO AQUI2',
+            descricao: 'TO AQUI',
+            bairro: 'TO AQUI',
+            rua: 'TO AQUI',
+            authorId: 678,
+            createdAt: Date(),
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROBLEM', problem });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROBLEM_ERROR', err });
+        })
+
     }
 };
